90c331ac0587648a351e80d0274ddfd4ead86e8b
NicoHC185 <nikohidalgo.castillo@gmail.com>
2024-11-08 20:33:47 -0300

    MOD: append env for product md

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -5,10 +5,14 @@ import * as joi from 'joi'
 interface EnvVars {
     PORT: number;
     ORDER_MICROSERVICES_HOST: string;
+    PRODUCT_PORT: number;
+    PRODUCT_MICROSERVICES_HOST: string;
 }
 
 const envsSchema = joi.object({
-    PORT: joi.number().required()
+    PORT: joi.number().required(),
+    PRODUCT_PORT: joi.number().required(),
+    PRODUCT_MICROSERVICES_HOST: joi.string().required()
 }).unknown(true)
 
 const { error, value } = envsSchema.validate(process.env)
@@ -20,5 +24,7 @@ if (error) {
 const envsVars: EnvVars = value
 
 export const envs = {
-    port: envsVars.PORT
+    port: envsVars.PORT,
+    productPort: envsVars.PRODUCT_PORT,
+    productHost: envsVars.PRODUCT_MICROSERVICES_HOST,
 }
