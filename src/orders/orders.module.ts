256d6fcd15307968aa2228f13636ab416544090a
NicoHC185 <nikohidalgo.castillo@gmail.com>
2024-11-08 20:34:28 -0300

    MOD: connect product ms witth tcp protocol

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { PRODUCT_SERVICES } from 'src/config/services';
+import { envs } from 'src/config/envs';
 
 @Module({
   controllers: [OrdersController],
   providers: [OrdersService],
+  imports: [
+    ClientsModule.register([
+      {
+        name: PRODUCT_SERVICES,
+        transport: Transport.TCP,
+        options: {
+          host: envs.productHost,
+          port: envs.productPort
+        }
+      },
+    ]),
+  ],
 })
-export class OrdersModule {}
+export class OrdersModule { }
